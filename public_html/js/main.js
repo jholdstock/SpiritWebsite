8b07dd060343f610c5330e6190c69a40042c1454
jholdstock <jam.jar69@gmail.com>
2015-05-23 13:05:40 +0100

    Layout data in a table and small visual changes

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -8,8 +8,7 @@
 	    slides: [
 	        { src: '/img/bg/1.jpg' },
 	        { src: '/img/bg/2.jpg' },
-	        { src: '/img/bg/3.jpg' },
-	        { src: '/img/bg/4.jpg' }
+	        { src: '/img/bg/3.jpg' }
 	    ]
 		});
 
