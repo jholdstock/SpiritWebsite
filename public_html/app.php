cc3d39cfda7308ec044f16819c89c790ace221be
jholdstock <jam.jar69@gmail.com>
2015-08-02 12:29:19 +0100

    json store gallery info

--- public_html/app.php
+++ public_html/app.php
@@ -22,6 +22,83 @@ $app->register(new UrlGeneratorServiceProvider());
 //
 // CONTEXT
 //
+
+$img = array (
+    "galleries" => array(
+        "automotive" => array(
+            "displayName" => "Automotive",
+            "images" => array(
+                "1" => array(
+                    "caption" => "automotive caption 1",
+                    "sub" => "automotive subcaption 1",
+                ),
+                "2" => array(
+                    "caption" => "automotive caption 2",
+                    "sub" => "automotive subcaption 2",
+                ),
+                "3" => array(
+                    "caption" => "automotive caption 3",
+                    "sub" => "automotive subcaption 3",
+                ),
+                "4" => array(
+                    "caption" => "automotive caption 4",
+                    "sub" => "automotive subcaption 4",
+                ),
+                "5" => array(
+                    "caption" => "automotive caption 5",
+                    "sub" => "automotive subcaption 5",
+                ),
+            )
+        ),
+         "concert" => array(
+            "displayName" => "Concert/Touring",
+            "images" => array(
+                "1" => array(
+                    "caption" => "concert caption 1",
+                    "sub" => "concert subcaption 1",
+                ),
+                "2" => array(
+                    "caption" => "concert caption 2",
+                    "sub" => "concert subcaption 2",
+                ),
+                "3" => array(
+                    "caption" => "concert caption 3",
+                    "sub" => "concert subcaption 3",
+                ),
+                "4" => array(
+                    "caption" => "concert caption 4",
+                    "sub" => "concert subcaption 4",
+                ),
+                "5" => array(
+                    "caption" => "concert caption 5",
+                    "sub" => "concert subcaption 5",
+                ),
+                "6" => array(
+                    "caption" => "concert caption 6",
+                    "sub" => "concert subcaption 6",
+                ),
+            )
+        ),
+        "corporate" => array(
+            "displayName" => "Corporate",
+            "images" => array(
+                "1" => array(
+                    "caption" => "corporate caption 1",
+                    "sub" => "corporate subcaption 1",
+                ),
+                "2" => array(
+                    "caption" => "corporate caption 2",
+                    "sub" => "corporate subcaption 2",
+                ),
+            )
+        ),
+    )
+);
+
+$fp = fopen('results.json', 'w');
+fwrite($fp, json_encode($img));
+fclose($fp);
+
 $context = array(
     "galleries" => array (
         new Gallery("automotive", "Automotive"),
